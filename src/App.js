b2b370e1dc8721eed39b8fb5ed040f2f03ae3029
Teejayyy <sirteejay007@gmail.com>
2021-07-09 13:56:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+
+ const      [user, setUser] = useState({});
+  const      [formErrors, setFormErrors] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="column left">
+        <div className="theForm">
+          <h3>Let's set up your account</h3>
+          <p>
+            Already have an account? <a href="#">Sign in</a>
+          </p>
+
+          <form className="login">
+            <div className="form-control">
+              <input type="text" placeholder="Your name" />
+            </div>
+            <div className="form-control">
+              <input
+                type="email"
+                id="email"
+                pattern=".+@+."
+                placeholder="Email address"
+                formNoValidate
+              />
+            </div>
+            <div className="form-control">
+              <select id="usertype">
+                <option>I would like to choose my usertype as</option>
+                <option>Developer</option>
+                <option>Engineer</option>
+                <option>Admin</option>
+                <option>DevOps</option>
+              </select>
+            </div>
+
+            <div className="form-control">
+              <input
+                type="password"
+                id="pass"
+                name="password"
+                minlength="8"
+                placeholder="Password"
+              />
+              <p className="minimum">Minimum 8 characters</p>
+            </div>
+
+            <button className="btn btn-block">Next</button>
+          </form>
+          <p className="click">
+            By clicking the "Next" button, you agree to creating a free account
+            and to <a href="#">Terms of Service </a>and{" "}
+            <a href="#">Privacy Policy</a>
+          </p>
+        </div>
+      </div>
+      <div className="column right">
+        <div className="dummy">
+          <h3>Dummy Heading</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur <br></br>adipisicing elit.
+            Assumenda, bla bla <br></br> reprehenderit nemo iusto unde rem
+            eligendi similique
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
